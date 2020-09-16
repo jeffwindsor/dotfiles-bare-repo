9fcc35806b39aedf6f28c4858231792ef2079d57
Windsor, Jeffrey <jwindsor@cj.com>
2020-09-15 20:36:38 -0700

    one_dark

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,8 +3,8 @@
 (setq
  user-full-name "Jeff Windsor"
  user-mail-address "jeff.windsor@gmail.com"
- doom-font (font-spec :family "JetBrains Mono" :size 14)
- doom-big-font (font-spec :family "Lexend Deca" :size 16)
+ doom-font (font-spec :family "JetBrains Mono" :size 16)
+ doom-big-font (font-spec :family "JetBrains Mono" :size 18)
  doom-theme 'doom-one
  display-line-numbers-type 'relative
  confirm-kill-emacs nil
