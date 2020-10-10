3760e7fa59046d8a991d9cad2c629a2b595985e6
Jeff Windsor <jeff.windsor@gmail.com>
2020-10-10 13:09:12 -0700

    laptop power management

--- .config/machine/pop-os/setup.sh
+++ .config/machine/pop-os/setup.sh
@@ -50,6 +50,7 @@ ai spotify-client vlc brave-browser
 sysdl='/etc/systemd/logind.conf'
 cat $sysdl \
     | sed -r 's/\#?HandleLidSwitchExternalPower=.*/HandleLidSwitchExternalPower=ignore/g' \
+    | sed -r 's/\#?HandleLidSwitch=.*/HandleLidSwitch=poweroff/g' \
     | sudo tee $sysdl
 
 #####################################################################################
