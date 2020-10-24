41ec122ca34151e358ac828a61f82ce53ba3e791
Jeff Windsor <jeff.windsor@gmail.com>
2020-10-24 15:46:02 -0700

    popos changes

--- .config/machine/pop-os/setup.sh
+++ .config/machine/pop-os/setup.sh
@@ -43,3 +43,24 @@ ai spotify-client vlc brave-browser
 # manual
 xdg-open https://mega.nz/sync
 xdg-open https://github.com/VSCodium/vscodium/releases
+
+# gnome theme
+mkdir -p $HOME/.themes
+mkdir -p $HOME/.icons
+
+# download and install solarized green theme
+FILE='Solarized-Dark-Green-3.36_2.0.3.zip'
+cd $HOME/Downloads
+    && wget "https://www.gnome-look.org/p/1308808/startdownload?file_id=1603172413&file_name=${FILE}.zip&file_type=application/zip&file_size=959489"
+    && unzip ${FILE}.zip
+    && mv ./${FILE}/* $HOME/.themes
+    && rm -rf ${FILE}
+    && cd -
+
+# download icons
+FILE='Solarized-Numix-FLAT-Green_2.0.4'
+cd $HOME/Downloads
+    && wget "https://www.pling.com/p/1309239/startdownload?file_id=1597908080&file_name=${FILE}.zip&file_type=application/zip&file_size=12511376"
+    && unzip ${FILE}.zip -d $HOME/.icons
+    && cd -
+
