50182e44ba3b04c23d96c971d6bf62849f046e44
Jeff Windsor <jeff.windsor@gmail.com>
2020-12-28 18:28:08 -0800

    spectrwm and polybar

--- .config/machine/arco/setup.sh
+++ .config/machine/arco/setup.sh
@@ -6,11 +6,6 @@
 set -e
 cd "$(dirname "${0}")"
 
-# packages
-#sudo pacman-mirrors --fasttrack
-#sudo pacman -Syyu
-#sudo pacman -S --noconfirm yay
-
 # pimp my shell
 (source ../clone-known-gits.sh)
 sudo pacman -S --noconfirm alacritty
@@ -29,14 +24,9 @@ sudo pacman -S --noconfirm go
 # editors
 sudo pacman -S --noconfirm neovim && (source ../install-neovim-plug.sh)
 sudo pacman -S --noconfirm emacs && (source ../install-doom-emacs.sh)
-yay -S --noconfirm vscodium-bin
-
-# extra
-yay -S --noconfirm spotify
-sudo pacman -S --noconfirm vlc brave-bin
 
 # requires rust
-#cargo install topgrade
+cargo install topgrade
 
 # manual
 xdg-open https://mega.nz/sync
