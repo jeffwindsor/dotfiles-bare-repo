3bd3cd7e7ecd32ee6a978efd4ac48867eae2ada0
Windsor, Jeffrey <jwindsor@cj.com>
2021-01-03 12:23:07 -0800

    merge universal links

--- .config/machine/link.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# git
-ln -sf "$XDG_CONFIG_HOME/gitignore" "$HOME/.gitignore"
-ln -sf "$XDG_CONFIG_HOME/git/cj.gitconfig" "$HOME/.gitconfig"
-# nvim
-ln -sf "$XDG_CONFIG_HOME/nvim/config.vim" "$HOME/.ideavim"
-# ssh
-ln -sf "$XDG_CONFIG_HOME/ssh/cj.config" "$HOME/.ssh/config"
