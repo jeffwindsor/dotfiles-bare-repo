a33f9d738432f82c4a07f69f73206021fa89ccee
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-03 20:04:01 -0800

    tender

--- .config/machine/arco/link.sh
+++ .config/machine/arco/link.sh
@@ -4,9 +4,15 @@ cd "$(dirname "${0}")"
 
 ln -sf "$PWD/zshenv" "$HOME/.zshenv"
 ln -sf "$PWD/zshrc" "$HOME/.zshrc"
+ln -sf "$PWD/systemd" "$HOME/.config/systemd"
+ln -sf "$PWD/alacritty" "$HOME/.config/alacritty"
+
 ln -sf "${HOME}/.config/gitignore" "$HOME/.gitignore"
 ln -sf "${HOME}/.config/git/personal.gitconfig" "$HOME/.gitconfig"
 ln -sf "${HOME}/.config/ssh/personal.config" "$HOME/.ssh/config"
+
+
+
 #ln -sf "${HOME}/.config/machine/arco/spectrwm.conf" "$HOME/.spectrwm.conf"
 #ln -sf "${HOME}/.config/machine/arco/spectrbar.sh" "$HOME/.config/spectrwm/spectrbar.sh"
 #ln -sf "${HOME}/.config/machine/arco/polybar.config" "$HOME/.config/polybar/config"
