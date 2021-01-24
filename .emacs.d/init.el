701a96de6570983e544d0eb0634f6f24a59dca8f
Windsor, Jeffrey <jwindsor@cj.com>
2021-01-23 16:30:10 -0800

    ivy menus

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,36 @@
+;; Packages
+(setq package-list '(
+		     doom-themes
+		     doom-modeline
+             evil
+             ivy))
+
+;; Packages: repositories
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Packages: Activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; Pacakges: Update your local package index
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Packages: Install all missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Packages: Configuration
+(require 'evil)
+(evil-mode t)
+(load-theme 'doom-one 't)
+(doom-modeline-mode 1)
+(ivy-mode 1)
+
+;; Visuals
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
@@ -5,34 +38,5 @@
 (set-fringe-mode 10)        ; Give some breathing room
 (menu-bar-mode -1)          ; Disable the menu bar
 
-
-(require 'package)
-
-; List the packages you want
-(setq package-list '(evil
-		     doom-themes))
-
-; Add Melpa as the default Emacs Package repository
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-; Activate all the packages (in particular autoloads)
-(package-initialize)
-
-; Update your local package index
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; Install all missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-
-(require 'evil)
-(evil-mode t)
-(load-theme 'doom-one 't)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;;Keys 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)     ;; Make ESC quit prompts
