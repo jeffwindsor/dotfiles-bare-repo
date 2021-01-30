363476a70084b632f7482bee8420d3a97e02759f
Jeff Windsor <jeffwindsor@jeffs-mbp.lan>
2021-01-29 18:12:02 -0800

    remove xdg

--- .config/machine/macos/link.sh
+++ .config/machine/macos/link.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-set -e
 cd "$(dirname "${0}")"
 
 
 # machine specific
-ln -sFf "$PWD/alacritty" "$XDG_CONFIG_HOME/alacritty"
-ln -sFf "$PWD/alfred" "$XDG_CONFIG_HOME/alfred"
-ln -sFf "$PWD/iterm" "$XDG_CONFIG_HOME/iterm"
-ln -sFf "$PWD/karabiner" "$XDG_CONFIG_HOME/karabiner"
+ln -sFf "$PWD/alacritty" "$HOME/.config/alacritty"
+ln -sFf "$PWD/alfred" "$HOME/.config/alfred"
+ln -sFf "$PWD/iterm" "$HOME/.config/iterm"
+ln -sFf "$PWD/karabiner" "$HOME/.config/karabiner"
 ln -sFf "$PWD/DefaultKeyBindings.dict" "$HOME/Library/KeyBindings/DefaultKeyBindings.dict"
 ln -sFf "$PWD/vscode/keybindings.json.macos" "$HOME/Library/Application Support/VSCodium/User/keybindings.json"
 ln -sFf "$PWD/vscode/settings.json.macos" "$HOME/Library/Application Support/VSCodium/User/settings.json"
@@ -15,7 +14,7 @@ ln -sFf "$PWD/zshenv" "$HOME/.zshenv"
 ln -sFf "$PWD/zshrc" "$HOME/.zshrc"
 
 # universal links
-ln -sf "$XDG_CONFIG_HOME/gitignore" "$HOME/.gitignore"
-ln -sf "$XDG_CONFIG_HOME/git/cj.gitconfig" "$HOME/.gitconfig"
-ln -sf "$XDG_CONFIG_HOME/nvim/config.vim" "$HOME/.ideavimrc"
-ln -sf "$XDG_CONFIG_HOME/ssh/cj.config" "$HOME/.ssh/config"
+ln -sf "$HOME/.config/gitignore" "$HOME/.gitignore"
+ln -sf "$HOME/.config/git/cj.gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/.config/nvim/config.vim" "$HOME/.ideavimrc"
+ln -sf "$HOME/.config/ssh/cj.config" "$HOME/.ssh/config"
