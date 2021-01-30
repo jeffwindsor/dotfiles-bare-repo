ed8c9d6e78032886ad16cef1df19c1e020df936e
Windsor, Jeffrey <jwindsor@cj.com>
2021-01-29 19:47:37 -0800

    split macos and macos-cj

--- .config/machine/macos-cj/macos/setup.sh
+++ .config/machine/macos-cj/macos/setup.sh
@@ -21,6 +21,17 @@ clone-if-missing(){
     [[ ! -d $2 ]] && git clone https://github.com/${1}/${2}.git $2
 }
 
+read -r -p "Clone Dotfiles? [y/n] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then            
+    pushd $HOME \
+        && git clone --bare https://github.com/jeffwindsor/dotfiles.git $HOME/.dotfiles \
+        && pushd $home/.dotfiles \
+        && git config --local status.showUntrackedFiles no \
+        && popd && popd
+fi 
+
 read -r -p "Add Homebrew? [y/n] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
@@ -151,6 +162,55 @@ then
     mas "iStat Menus", id: 1319778037
     mas "Microsoft OneNote", id:784801555
 fi
+
+read -r -p "Add CJ specific packages? [y/n] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then            
+    brew tap InstantClientTap/instantclient
+
+packages=(
+cassandra
+clojure
+kafka
+maven
+maven-completion
+openjdk
+parquet-tools
+sbt
+scala
+selenium-server-standalone
+)
+
+casks=(
+instantclient-basic
+instantclient-sqlplus
+instantclient-sqlplus
+java8
+keybase
+microsoft-teams
+)
+
+    for p in "${packages[@]}"; do install-package "$p"; done;
+    for p in "${casks[@]}"; do install-cask "$p"; done;
+fi
+
+
+read -r -p "Set zsh as default? [y/n] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then            
+    sudo cat > /etc/shells <<EOL
+/bin/sh
+/usr/local/bin/bash
+/usr/local/bin/fish
+/usr/local/bin/zsh
+EOL
+
+    # set default shell
+    chsh -s "$(which zsh)"
+fi 
+
 
 read -r -p "Create SSH key? [y/n] " response
 response=${response,,}    # tolower
