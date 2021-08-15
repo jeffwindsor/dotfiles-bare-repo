9a97e314dd3359035e2aeb478018d2b295db5883
jeff windsor <jeff.windsor@gmail.com>
2021-08-15 10:07:58 -0700

    fixing

--- .config/os/arch/link.sh
+++ .config/os/arch/link.sh
@@ -7,10 +7,6 @@ ln -sf $PWD/zshrc $HOME/.zshrc
 
 mkdir -p $HOME/.xmonad
 ln -sf $PWD/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
-
-rm -rf $HOME/.xmonad/scripts/ 
 ln -sf $PWD/xmonad/scripts/ $HOME/.xmonad/
-
-rm -rf $HOME/.xmonad/polybar/ 
 ln -sf $PWD/xmonad/polybar/ $HOME/.xmonad/
 
