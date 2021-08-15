f9386f24fc6c9b467a601a198985289e3870793c
jeff windsor <jeff.windsor@gmail.com>
2021-08-15 10:03:48 -0700

    fixing

--- .config/os/arch/link.sh
+++ .config/os/arch/link.sh
@@ -4,3 +4,13 @@ cd "$(dirname "${0}")"
 
 ln -sf $PWD/zshenv $HOME/.zshenv
 ln -sf $PWD/zshrc $HOME/.zshrc 
+
+mkdir -p $HOME/.xmonad
+ln -sf $PWD/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
+
+rm -rf $HOME/.xmonad/scripts/ 
+ln -sf $PWD/xmonad/scripts/ $HOME/.xmonad/
+
+rm -rf $HOME/.xmonad/polybar/ 
+ln -sf $PWD/xmonad/polybar/ $HOME/.xmonad/
+
--- .config/os/arch/xmonad/polybar/launch.sh
+++ .config/os/arch/xmonad/polybar/launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+cd "$(dirname "${0}")"
 
 # More info : https://github.com/jaagr/polybar/wiki
 killall -q polybar
@@ -11,19 +12,19 @@ count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
 # top bar
 if [ $count = 1 ]; then
   m=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  MONITOR=$m polybar --reload mainbar-xmonad -c ~/.config/polybar/config &
+  MONITOR=$m polybar --reload mainbar-xmonad -c $PWD/config &
 else
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-xmonad -c ~/.config/polybar/config &
+    MONITOR=$m polybar --reload mainbar-xmonad -c $PWD/config &
   done
 fi
 
 # bottom bar
 if [ $count = 1 ]; then
   m=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  MONITOR=$m polybar --reload mainbar-xmonad-extra -c ~/.config/polybar/config &
+  MONITOR=$m polybar --reload mainbar-xmonad-extra -c $PWD/config &
 else
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-xmonad-extra -c ~/.config/polybar/config &
+    MONITOR=$m polybar --reload mainbar-xmonad-extra -c $PWD/config &
   done
 fi
